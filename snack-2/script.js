aeb5de9e78e0e901bf593eea2c8f0c5476127605
lisadelladucata <lisa.delladucata@gmail.com>
2024-12-12 18:04:17 +0100

    feat:add-snack-2

--- snack-2/script.js
+++ snack-2/script.js
@@ -3,3 +3,48 @@
 // Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 // Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 // Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+
+// genero l'array con proprietà punti e falli settati a 0 
+let teams=[
+    {
+        name:"Juventus",
+        point: 0,
+        foul: 0,
+    },
+    {
+        name:"Roma",
+        point: 0,
+        foul: 0,
+    },
+    {
+        name:"Inter",
+        point: 0,
+        foul: 0,
+    }
+]
+
+//creo la funzione per generare numeri random da sostituire alle proprietà
+
+function randomNumber(){
+    teams.forEach(teams =>{
+    teams.point= Math.floor(Math.random() * 51);
+    teams.foul=Math.floor(Math.random() * 51)
+    })
+}
+
+randomNumber();
+
+console.log(teams)
+
+//funzione per creare nuova array con solo proprietà nome e falli subiti
+
+let teamsNameFoul = teams.map(teams => {
+    return{
+        name: teams.name,
+        foul: teams.foul,
+    }
+})
+
+console.log(teamsNameFoul)
+
